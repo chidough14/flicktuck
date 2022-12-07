5aad553d35cf26ed1e6fde56d819f3ef7a882736
Chidozie <chidough14@gmail.com>
2022-12-07 13:36:54 +0100

    Google authentication/save user in db

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,15 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
-import { GoogleLogin, GoogleLogout } from "react-google-login"
+import { GoogleLogin, googleLogout } from "@react-oauth/google"
 import { AiOutlineLogout } from "react-icons/ai"
 import { BiSearch } from "react-icons/bi"
 import { IoMdAdd } from "react-icons/io"
 import Logo from "../utils/tiktik-logo.png"
+import { createOrGetUser } from '../utils'
 
 const Navbar = () => {
+  const user = false
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href="/">
@@ -21,6 +23,23 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>
+        SEARCH
+      </div>
+
+      <div>
+        {
+          user ? (
+            <div>Logged in</div>
+          ) : (
+            <GoogleLogin
+              onSuccess={(response) => createOrGetUser(response)}
+              onError={() => { console.log("error")}}
+            />
+          )
+        }
+      </div>
     </div>
   )
 }
