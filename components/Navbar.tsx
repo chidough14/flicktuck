7cba8fc8c6bd92937e4f3f2084173db06db183fa
Chidozie <chidough14@gmail.com>
2022-12-07 14:06:36 +0100

    Zustand setup/logout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,9 +8,11 @@ import { BiSearch } from "react-icons/bi"
 import { IoMdAdd } from "react-icons/io"
 import Logo from "../utils/tiktik-logo.png"
 import { createOrGetUser } from '../utils'
+import useAthStore from '../store/authStore'
 
 const Navbar = () => {
-  const user = false
+  const {userProfile, addUser, removeUser} = useAthStore()
+
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href="/">
@@ -30,11 +32,47 @@ const Navbar = () => {
 
       <div>
         {
-          user ? (
-            <div>Logged in</div>
+          userProfile ? (
+            <div className='flex gap-5 md:gap-10'>
+              <Link href="/">
+                <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+                  <IoMdAdd className='text-xl' /> {` `}
+                  <span className='hidden md:block'>
+                    Upload
+                  </span>
+                </button>
+              </Link>
+
+              {
+                userProfile.image && (
+                  <Link href="/">
+                    <>
+                      <Image
+                        width={42}
+                        height={42}
+                        className="rounded-full cursor-pointer"
+                        src={userProfile.image}
+                        alt="Profile"
+                      />
+                    </>
+                  </Link>
+                )
+              }
+
+              <button
+                type='button'
+                className='px-2'
+                onClick={() => {
+                  googleLogout()
+                  removeUser()
+                }}
+              >
+                <AiOutlineLogout color='red' fontSize={21}/>
+              </button>
+            </div>
           ) : (
             <GoogleLogin
-              onSuccess={(response) => createOrGetUser(response)}
+              onSuccess={(response) => createOrGetUser(response, addUser)}
               onError={() => { console.log("error")}}
             />
           )
---
+++ store/authStore.ts
@@ -0,0 +1,17 @@
+import create from 'zustand'
+import { persist } from 'zustand/middleware'
+import axios from 'axios'
+
+const authStore = (set: any) => ({
+  userProfile: null,
+  addUser: (user: any) => set({userProfile: user}),
+  removeUser: () => set({userProfile: null}),
+})
+
+const useAthStore = create(
+  persist(authStore, {
+    name: "auth"
+  })
+)
+
+export default useAthStore
